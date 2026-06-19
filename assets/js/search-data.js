ec2b58bc75732e779ae1e9561ea9941ccc534249
longxinchen-ai <gongjian_ai@foxmail.com>
2026-06-19 14:45:31 +0000

    Deploying to gh-pages from @ longxinchen-ai/longxinchen-ai.github.io@540b5fd2305b4c53a603e9366967828d2e7f9347 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -150,7 +150,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6C%6F%6E%67%78%69%6E%63%68%65%6E.%61%69@%6F%75%74%6C%6F%6F%6B.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -174,11 +174,18 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-zhihu',
+        title: 'Zhihu',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.zhihu.com/people/longxinchen/posts", "_blank");
+        },
+      },{
+        id: 'social-wechat',
+        title: 'Wechat',
+        section: 'Socials',
+        handler: () => {
+          window.open("#", "_blank");
         },
       },{
       id: 'light-theme',
